29a972eec757c39ea8cd8321def0136f9dda96a1
YIRer <xeroonelove@gmail.com>
2023-06-05 19:27:13 +0900

    feat:  eslint, prettier 등 환경 설정 및 초기화

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: 'loose', // required to make Konva & react-konva work
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
